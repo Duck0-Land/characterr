8c970dfe90bfecfc284aec8300261003781ed5e7
Duck0-Land <erhilston@gmail.com>
2023-03-06 19:18:40 -0500

    shit2

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+import { createAvatar } from '@dicebear/core';
+import { lorelei } from '@dicebear/collection';
+
+const avatar = createAvatar(lorelei, {
+  seed: 'John Doe',
+  // ... other options
+});
+
+const svg = avatar.toString();
