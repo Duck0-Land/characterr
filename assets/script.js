84368db9936dc0beae0e38eca0a5c0b30011941f
Duck0-Land <erhilston@gmail.com>
2023-03-09 19:51:55 -0500

    test1

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
-const aframe = document.getElementById("frame");
-const gen = document.getElementById("gen");
+let aframe = document.getElementById("frame");
+let gen = document.getElementById("gen");
 
 
 // --
 // Main function
 function generate() {
-    const id = Math.random();
-    const url = `https://avatars.dicebear.com/api/adventurer/${id}.svg`
+    let id = Math.random();
+    let url = `https://avatars.dicebear.com/api/adventurer/${id}.svg`
     aframe.src = url;
 }
 
 // main event
 gen.addEventListener('click', function(e) {
-    e.preventDefault();
+
     generate();
 });
 
